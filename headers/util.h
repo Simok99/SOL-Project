e1c581980ed90239230d5e5d0622f34663e77564
Simok99 <simok99@libero.it>
2021-09-22 19:02:26 +0200

    added isNumber utility function

--- headers/util.h
+++ headers/util.h
@@ -125,4 +125,27 @@ static inline void print_error(const char *str, ...)
     free(p);
 }
 
+/** 
+ * \brief Controlla se la stringa passata come primo argomento e' un numero.
+ * \return  0 ok  1 non e' un numero   2 overflow/underflow
+ */
+static inline int isNumber(const char *s, long *n)
+{
+    if (s == NULL)
+        return 1;
+    if (strlen(s) == 0)
+        return 1;
+    char *e = NULL;
+    errno = 0;
+    long val = strtol(s, &e, 10);
+    if (errno == ERANGE)
+        return 2; // overflow/underflow
+    if (e != NULL && *e == (char)0)
+    {
+        *n = val;
+        return 0; // successo
+    }
+    return 1; // non e' un numero
+}
+
 #endif
